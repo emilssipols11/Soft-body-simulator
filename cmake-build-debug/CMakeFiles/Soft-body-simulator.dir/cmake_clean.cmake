a703b113e24fbf7f988589d144fced9b78f252db
Emils <emils.sipols95@gmail.com>
2021-04-19 20:57:04 +0200

    minor cmake changes

---
+++ cmake-build-debug/CMakeFiles/Soft-body-simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Soft-body-simulator.dir/main.cpp.obj"
+  "Soft-body-simulator.exe"
+  "Soft-body-simulator.exe.manifest"
+  "Soft-body-simulator.pdb"
+  "libSoft-body-simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Soft-body-simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Soft-body.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Soft-body.
+# This may be replaced when dependencies are built.
